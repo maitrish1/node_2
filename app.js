ed336dc7c3bb2fc43975eb99e6381fe2b860dbcc
Maitrish Mukherjee <maitrish03@gmail.com>
2024-06-04 14:05:10 +0530

    add routes

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const http = require("http");
 
 const server= http.createServer((req,res)=>{
-    console.log('Maitrish')
+    res.setHeader('Content-Type',"text/html")
+    let url=req.url
+    if(url==='/home'){
+        res.write("<h1>Welcome home</h1>")
+    }
+    if(url==='/about'){
+        res.write("<h1>Welcome to About Us page</h1>")
+    }
+    if(url==='/node'){
+        res.write("<h1>Welcome to my Node Js project</h1>")
+    }
+
+    res.end()
 });
 
 server.listen(4000)
