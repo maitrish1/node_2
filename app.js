413c91ee098c03b31a52029fd1e197626f7d88ed
Maitrish Mukherjee <maitrish03@gmail.com>
2024-06-06 12:48:22 +0530

    added message showing functionality

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
-const http = require("http");
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const server= http.createServer((req,res)=>{
-    res.setHeader('Content-Type',"text/html")
-    let url=req.url
-    if(url==='/home'){
-        res.write("<h1>Welcome home</h1>")
-    }
-    if(url==='/about'){
-        res.write("<h1>Welcome to About Us page</h1>")
-    }
-    if(url==='/node'){
-        res.write("<h1>Welcome to my Node Js project</h1>")
-    }
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
 
-    res.end()
+  if (url === '/') {
+    fs.readFile('message.txt', 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+        data = 'No messages yet';
+      }
+      res.write('<html>');
+      res.write('<head><title>Enter Message</title><head>');
+      res.write('<body>');
+      res.write(`<h1>Messages:</h1><pre>${data}</pre>`);
+      res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
+      res.write('</body>');
+      res.write('</html>');
+      return res.end();
+    });
+  }
+
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.readFile('message.txt', 'utf8', (err, data) => {
+        if (err) {
+          console.log(err);
+          data = '';
+        }
+        const updatedData = message + '\n' + data;
+        fs.writeFile('message.txt', updatedData, (err) => {
+          if (err) {
+            console.log(err);
+          }
+          res.statusCode = 302;
+          res.setHeader('Location', '/');
+          return res.end();
+        });
+      });
+    });
+  }
 });
 
-server.listen(4000)
+server.listen(3000);
