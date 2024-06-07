196d9679d936dd6198601437fa3793528ba0a8b8
Maitrish Mukherjee <maitrish03@gmail.com>
2024-06-07 13:35:42 +0530

    add productand form

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const http = require("http");
-
 const express = require("express");
-
+const bodyParser=require('body-parser')
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('in the middleware')
-    next() //Allows req to continue to next middleware
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/add-product',(req,res,next)=>{
+    res.send("<form action='/product' method='POST'><input type='text' name='title'/> <input type='text' name='size'/><button type='submit'>Add product</button></form>")
 })
 
-app.use((req,res,next)=>{
-    console.log('in the middleware 2')
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+app.use('/',(req,res,next)=>{
     res.send('<h1>HELLO Its me</h1>')
 })
+
 
 app.listen(3000);
