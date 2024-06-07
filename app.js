b47dcf0cfbda2fde294daa8c90a2b3cc22ea7638
Maitrish Mukherjee <maitrish03@gmail.com>
2024-06-07 12:19:22 +0530

    add express

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const http = require('http');
-const  routes=require('./routes')
-const server = http.createServer(routes);
-//console.log('kjkjkj)
-server.listen(3000);
+const http = require("http");
+
+const express = require("express");
+
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log('in the middleware')
+    next() //Allows req to continue to next middleware
+})
+
+app.use((req,res,next)=>{
+    console.log('in the middleware 2')
+    res.send('<h1>HELLO Its me</h1>')
+})
+
+app.listen(3000);
