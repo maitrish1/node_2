fb6ce95b1fb8caf120fbc152c22a4aa5293803bc
Maitrish Mukherjee <maitrish03@gmail.com>
2024-06-07 19:04:30 +0530

    add routes and filters

--- app.js
+++ app.js
@@ -2,19 +2,15 @@ const express = require("express");
 const bodyParser=require('body-parser')
 const app = express();
 
+const adminRoutes=require('./routes/admin')
+const shopRoutes=require('./routes/shop')
+app.use('/admin',adminRoutes)
+app.use(shopRoutes)
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not foud</h1>')
+})
+
 app.use(bodyParser.urlencoded({extended:false}))
-
-app.use('/add-product',(req,res,next)=>{
-    res.send("<form action='/product' method='POST'><input type='text' name='title'/> <input type='text' name='size'/><button type='submit'>Add product</button></form>")
-})
-
-app.use('/product',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/')
-})
-app.use('/',(req,res,next)=>{
-    res.send('<h1>HELLO Its me</h1>')
-})
-
 
 app.listen(3000);
